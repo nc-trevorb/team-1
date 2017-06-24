e712b9e8dec1872a2e8ff35ee504dd3b82f060d1
Benjamin Trevor <trevorb@nextcapital.com>
2017-06-24 13:05:36 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Ben T', email: 'trevorb@nextcapital.com')
+User.create(name: 'Jason S', email: 'shanmughamj@nextcapital.com')
+User.create(name: 'Chris P', email: 'passolanoc@nextcapital.com')
+User.create(name: 'Zhengchao L', email: 'liuz@nextcapital.com')
+
+
+Restaurant.create(name: 'Potbelly')
+Restaurant.create(name: 'Jimmy Johns')
+Restaurant.create(name: 'Subway')
+Restaurant.create(name: 'Chipotle')
+Restaurant.create(name: 'Pret')
+Restaurant.create(name: 'McDonalds')
